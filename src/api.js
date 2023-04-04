585c25e891922820f882fede395bcc1b4e9de679
Kashyap Patel <patel123kashyap@gmail.com>
2023-04-05 04:34:24 +0530

    initial commit

---
+++ src/api.js
@@ -0,0 +1,12 @@
+export const geoApiOptions = {
+  method: "GET",
+  headers: {
+    "X-RapidAPI-Key": "4656013120msh82bc3b7cfaa9ddfp1ec174jsnf0876cad3bae",
+    "X-RapidAPI-Host": "wft-geo-db.p.rapidapi.com",
+  },
+};
+
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
+
+export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
+export const WEATHER_API_KEY = "9e697729d4f73f40b403d8a61268b91d";
